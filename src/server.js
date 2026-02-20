722d1b4ef40038f3722b5ff856f80ce19d867209
LucasCordeir0 <luc4scdr@gmail.com>
2026-02-19 22:27:43 -0300

    feat: initial express server setup

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('clinical Flow webApp está rodando 🚀');
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
